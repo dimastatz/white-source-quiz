5137a189784d29a522043175ada21ace7aa2f4f9
Dima Statz <dima.statz@cloudlock.com>
2018-11-15 21:55:25 +0200

    add sbm solution in python

--- sbm-sequence.py
+++ sbm-sequence.py
@@ -3,24 +3,26 @@ app = Flask(__name__)
 
 
 def find_sbm(seq):
-    min1, max1, seq = scan(seq[1:], seq[0], lambda x, y: x < y)
-    max2, min2, seq = scan(seq[1:], max1, lambda x, y: x > y)
-    return [min1, max2, min2] if max2 and min2 and min1 < max2 > min2 else []
-
-
-def scan(seq, start, compare):
-    if not start or len(seq) == 0:
-        return start, None, seq
-    elif compare(start, seq[0]):
-        return start, seq[0], seq
+    if seq:
+        min1, seq1 = scan(seq, lambda s: len(s) < 2 or s[0] < s[1])
+        max1, seq1 = scan(seq1, lambda s: len(s) < 2 or s[0] > s[1])
+        result = min1 and max1 and scan(seq1, lambda s: min1 < s[0] < max1)[0] is not None
+        return find_sbm(seq1) if not result else result
     else:
-        return scan(seq[1:], seq[0] if not compare(start, seq[0]) else start, compare)
+        return False
+
+
+def scan(seq, stop_condition):
+    if not seq or stop_condition(seq):
+        return None if not seq else seq[0], seq[1:] if seq else []
+    else:
+        return scan(seq[1:], stop_condition)
 
 
 @app.route('/server', methods=['POST'])
 def find_sbm_api():
     content = request.json['seq']
-    return 'Result: {}\n'.format(len(find_sbm(content)) > 0)
+    return 'Result: {}\n'.format(find_sbm(content))
 
 
 if __name__ == '__main__':
