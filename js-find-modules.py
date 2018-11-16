3f709699f2b55d35c01bb1b0c262c8e574e7c37a
Dima Statz <dima.statz@cloudlock.com>
2018-11-16 08:12:36 +0200

    add find modules

---
+++ js-find-modules.py
@@ -0,0 +1,27 @@
+import os
+from slimit import ast
+from slimit.parser import Parser
+from slimit.visitors import nodevisitor
+
+
+def is_require_local(node):
+    return isinstance(node, ast.FunctionCall) \
+           and node.identifier.value == 'require' \
+           and node.args[0].value.startswith("'.")
+
+
+def parse_file(file_name):
+    with open(file_name) as fd:
+        tree = Parser().parse(fd.read())
+        return [x.args[0].value for x in nodevisitor.visit(tree) if is_require_local(x)]
+
+
+def parse_folder_files(folder):
+    files = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith(".js")]
+    return [parse_file(x) for x in files]
+
+
+if __name__ == '__main__':
+    path = '/Users/dimastatz/Documents/dev/sit/white-source-quiz/'
+    result = parse_folder_files(path)
+    print(result)
