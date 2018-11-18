d613aa442e3974312bd178974a3d0f6a80f9dd01
Dima Statz <dima.statz@cloudlock.com>
2018-11-18 21:18:47 +0200

    fix bug

--- sbm-sequence-2.py
+++ sbm-sequence-2.py
@@ -4,26 +4,15 @@ app = Flask(__name__)
 
 def find_sbm(seq):
     lst = sorted(enumerate(seq), key=lambda x: x[1])  # sort O(n*log(n))
-    return scan_for_trend(lst)  # scan O(n)
+    return scan(lst)  # scan O(n)
 
 
-def is_stop_cond(min_n, seq, max_n):
-    return min_n and max_n and max_n[0] > seq[0][0] > min_n[0] \
-           and min_n[1] != seq[0][1] and max_n != seq[0][1] and max_n[1] != min_n[1]
-
-
-def scan_for_trend(seq, min_n=None, max_n=None):
-    if not seq:
+def scan(seq):
+    if len(seq) < 3:
         return False
-    elif is_stop_cond(min_n, seq, max_n):
+    elif seq[0][1] < seq[1][1] < seq[2][1] and seq[0][0] < seq[1][0] > seq[2][0]:
         return True
-
-    if not min_n or seq[0][0] <= min_n[0]:
-        return scan_for_trend(seq[1:], seq[0])
-    elif not max_n or seq[0][0] >= max_n[0]:
-        return scan_for_trend(seq[1:], min_n, seq[0])
-    else:
-        return scan_for_trend(seq[1:], min_n, max_n)
+    return scan(seq[1:])
 
 
 @app.route('/server', methods=['POST'])
@@ -33,3 +22,17 @@ def find_sbm_api():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
+
+
+# print(find_sbm([7, 8, 1, 2, 2]))
+# print(find_sbm([7, 8, 1, 3, 2]))
+# print(find_sbm([7, 9, 1, 2, 8]))
+#
+# print(find_sbm([1, 2, 3]))
+# print(find_sbm([1, 2, 3, 2]))
+# print(find_sbm([1, 2, 3, 7]))
+# print(find_sbm([4, 1, 7, 8, 7, 2]))
+# print(find_sbm([7, 2, 2, 3, 1, 2, 1, 2, 3, 7]))
+# print(find_sbm([1, 2, 2, 4, -11, 1, 5, 1, 5, 7, 7, 10, 11, 12, 40, -10, -5]))
+
+
