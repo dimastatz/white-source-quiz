9baccc9e894a12dba7bf1e687ade4b4b27477804
Dima Statz <dima.statz@cloudlock.com>
2018-11-23 14:03:58 +0200

    O(n)

--- sbm-sequence-2.py
+++ sbm-sequence-2.py
@@ -3,29 +3,42 @@ app = Flask(__name__)
 
 
 def find_sbm(seq):
-    lst = sorted(enumerate(seq), key=lambda x: (x[1], -x[0]))  # sort O(n*log(n))
-    return scan(lst)  # scan O(n)
+    return scan(seq, set(), False)
 
 
-def scan(seq):
-    if len(seq) < 3:
+def scan(seq, known_range, is_up_trend):
+    if not seq or len(seq) < 2:
         return False
-    elif seq[0][1] < seq[1][1] < seq[2][1] and seq[0][0] < seq[1][0] > seq[2][0]:
+    elif seq[1] in known_range:  # 0(1)
         return True
-    return scan(seq[1:])
+    else:
+        known_range = add_set(known_range, is_up_trend, seq[0], seq[1])
+        return scan(seq[1:], known_range, seq[0] < seq[1])
+
+
+def add_set(s, include_min, min_n, max_n):
+    r = range(min_n, max_n) if include_min else range(min_n + 1, max_n)
+    return s | set(r)
+
+
+@app.route('/server', methods=['POST'])
+def find_sbm_api():
+    return 'Result: {}\n'.format(find_sbm(request.json['seq']))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
 
 
 print(find_sbm([8, 10, 9, 8]))
-
-# print(find_sbm([7, 8, 1, 2, 2]))
-# print(find_sbm([7, 8, 1, 3, 2]))
-# print(find_sbm([7, 9, 1, 2, 8]))
-#
-# print(find_sbm([1, 2, 3]))
-# print(find_sbm([1, 2, 3, 2]))
-# print(find_sbm([1, 2, 3, 7]))
-# print(find_sbm([4, 1, 7, 8, 7, 2]))
-# print(find_sbm([7, 2, 2, 3, 1, 2, 1, 2, 3, 7]))
-# print(find_sbm([1, 2, 2, 4, -11, 1, 5, 1, 5, 7, 7, 10, 11, 12, 40, -10, -5]))
+print(find_sbm([7, 8, 1, 2, 2]))
+print(find_sbm([7, 8, 1, 3, 2]))
+print(find_sbm([7, 9, 1, 2, 8]))
+print(find_sbm([1, 2, 3]))
+print(find_sbm([1, 2, 3, 2]))
+print(find_sbm([1, 2, 3, 7]))
+print(find_sbm([4, 1, 7, 8, 7, 2]))
+print(find_sbm([7, 2, 2, 3, 1, 2, 1, 2, 3, 7]))
+print(find_sbm([1, 2, 2, 4, -11, 1, 5, 1, 5, 7, 7, 10, 11, 12, 40, -10, -5]))
 
 
